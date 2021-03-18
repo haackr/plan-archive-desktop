e9d2cc3942fbc2deab14323fda0a9e1346bd36f8
Ryan Haack <ryan@haackr.com>
2021-03-17 19:49:44 -0600

    Add Chackra UI

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import { client } from "./apolloClient";
 import SchoolList from "./components/SchoolList";
 
 const App = () => {
   return (
     <>
-      <ApolloProvider client={client}>
-        <h2>React up and running</h2>
-        <p>This is a react app.</p>
-        <SchoolList />
-      </ApolloProvider>
+      <ChakraProvider>
+        <ApolloProvider client={client}>
+          <h2>React up and running</h2>
+          <p>This is a react app.</p>
+          <SchoolList />
+        </ApolloProvider>
+      </ChakraProvider>
     </>
   );
 };
